891e02233c4d2bb0799dde2543323e1fea37362c
amine <amine.kacem1337@gmail.com>
2023-04-30 17:39:52 +0200

    okkk

--- welcome.php
+++ welcome.php
@@ -173,10 +173,15 @@ $stmt->execute();
   echo'     <div class="media g-mb-30 media-comment">';
   echo'          <div class="media-body u-shadow-v18 g-bg-secondary g-pa-30">';
   echo'           <div class="g-mb-15">';
-  echo'             <h5 class="h5 g-color-gray-dark-v1 mb-0">John Doe</h5>';
-  echo'             <span class="g-color-gray-dark-v4 g-font-size-12">5 days ago</span>';
+
+  if ($row['first_name'] !== NULL && $row['last_name'] !== NULL) {
+    echo '<h5 class="h5 g-color-gray-dark-v1 mb-0"><small>Publié par ' . htmlspecialchars($row['first_name']) . ' ' . htmlspecialchars($row['last_name']) . ' le ' . $row['created_at'] . '</small></h5>';
+} else {
+    echo '<h5 class="h5 g-color-gray-dark-v1 mb-0"><small>Publié le ' . $row['created_at'] . '</small>';
+}
   echo'           </div>';
   echo'           <p>'.htmlspecialchars($row['title']) .'</p>';   
+  echo'           <p>'.htmlspecialchars($row['content']) .'</p>';   
   echo'           <ul class="list-inline d-sm-flex my-0">';
   echo'             <li class="list-inline-item g-mr-20">';
   echo'               <a class="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover" href="#!">';
@@ -190,17 +195,13 @@ $stmt->execute();
                   34
                 </a>
               </li>';
-  echo'             <li class="list-inline-item ml-auto">';
-  echo'               <a class="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover" href="#!">';
-  echo'                 <i class="fa fa-reply g-pos-rel g-top-1 g-mr-3"></i>';
-  echo'                 Reply';
-  echo'               </a>';
-  echo'             </li>';
+ 
   echo'           </ul>';
   echo'         </div>';
   echo'     </div>';
 
   }
+  
   ?>
         </div>
 
